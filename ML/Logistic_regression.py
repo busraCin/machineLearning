a2c057863e89a7749221612da177a721ec02dbca
Büşra Cin <69642923+busraCin@users.noreply.github.com>
2023-07-26 22:13:42 +0300

    update

--- ML/Logistic_regression.py
+++ ML/Logistic_regression.py
@@ -12,7 +12,8 @@ import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import RobustScaler
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, classification_report, plot_roc_curve
+from sklearn.model_selection import train_test_split, cross_validate
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
@@ -94,3 +95,98 @@ log_model.coef_
 y_pred = log_model.predict(X)
 y_pred[0:10]
 y[0:10]
+
+# Model Evaluation
+def plot_confusion_matrix(y, y_pred):
+    acc = round(accuracy_score(y, y_pred), 2)
+    cm = confusion_matrix(y, y_pred)
+    sns.heatmap(cm, annot=True, fmt=".0f")
+    plt.xlabel('y_pred')
+    plt.ylabel('y')
+    plt.title('Accuracy Score: {0}'.format(acc), size=10)
+    plt.show()
+
+plot_confusion_matrix(y, y_pred)
+print(classification_report(y, y_pred))
+# Accuracy: 0.78
+# Precision: 0.74
+# Recall: 0.58
+# F1-score: 0.65
+
+# ROC AUC
+y_prob = log_model.predict_proba(X)[:, 1]
+roc_auc_score(y, y_prob)
+# 0.84
+
+
+# Model Validation: Holdout
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.20, random_state=17)
+log_model = LogisticRegression().fit(X_train, y_train)
+y_pred = log_model.predict(X_test)
+y_prob = log_model.predict_proba(X_test)[:, 1]
+print(classification_report(y_test, y_pred))
+
+# Accuracy: 0.78
+# Precision: 0.74
+# Recall: 0.58
+# F1-score: 0.65
+
+# Accuracy: 0.77
+# Precision: 0.79
+# Recall: 0.53
+# F1-score: 0.63
+
+plot_roc_curve(log_model, X_test, y_test)
+plt.title('ROC Curve')
+plt.plot([0, 1], [0, 1], 'r--')
+plt.show()
+
+# AUC
+roc_auc_score(y_test, y_prob)
+
+# Model Validation: 10-Fold Cross Validation
+y = df["Outcome"]
+X = df.drop(["Outcome"], axis=1)
+
+log_model = LogisticRegression().fit(X, y)
+
+cv_results = cross_validate(log_model,
+                            X, y,
+                            cv=5,
+                            scoring=["accuracy", "precision", "recall", "f1", "roc_auc"])
+
+
+
+# Accuracy: 0.78
+# Precision: 0.74
+# Recall: 0.58
+# F1-score: 0.65
+
+# Accuracy: 0.77
+# Precision: 0.79
+# Recall: 0.53
+# F1-score: 0.63
+
+
+cv_results['test_accuracy'].mean()
+# Accuracy: 0.7721
+
+cv_results['test_precision'].mean()
+# Precision: 0.7192
+
+cv_results['test_recall'].mean()
+# Recall: 0.5747
+
+cv_results['test_f1'].mean()
+# F1-score: 0.6371
+
+cv_results['test_roc_auc'].mean()
+# AUC: 0.8327
+
+# Prediction for A New Observation
+X.columns
+
+random_user = X.sample(1, random_state=45)
+log_model.predict(random_user)
