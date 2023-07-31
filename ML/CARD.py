38e6060f910a093b386017ade63fc11189cd693b
Büşra Cin <69642923+busraCin@users.noreply.github.com>
2023-07-31 15:59:14 +0300

    CARD

--- ML/CARD.py
+++ ML/CARD.py
@@ -108,3 +108,123 @@ cv_results = cross_validate(cart_final,
 cv_results['test_accuracy'].mean() #0.75
 cv_results['test_f1'].mean() #0.61
 cv_results['test_roc_auc'].mean() #0.79
+
+#Feature Importance
+cart_final.feature_importances_
+def plot_importance(model, features, num=len(X), save=False):
+    feature_imp = pd.DataFrame({'Value': model.feature_importances_, 'Feature': features.columns})
+    plt.figure(figsize=(10, 10))
+    sns.set(font_scale=1)
+    sns.barplot(x="Value", y="Feature", data=feature_imp.sort_values(by="Value",
+                                                                     ascending=False)[0:num])
+    plt.title('Features')
+    plt.tight_layout()
+    plt.show()
+    
+    if save:
+        plt.savefig('importances.png')
+
+plot_importance(cart_final, X, num=len(X)) 
+
+#Analyzing Model Complexity with Learning Curves
+train_score, test_score = validation_curve(cart_final, X, y,
+                                           param_name="max_depth",
+                                           param_range=range(1, 11), 
+                                           scoring="roc_auc",
+                                           cv=10)
+
+
+mean_train_score = np.mean(train_score, axis=1)
+mean_test_score = np.mean(test_score, axis=1)
+
+plt.plot(range(1, 11), mean_train_score,
+         label="Training Score", color='b')
+
+plt.plot(range(1, 11), mean_test_score,
+         label="Validation Score", color='g')
+
+plt.title("Validation Curve for CART")
+plt.xlabel("Number of max_depth")
+plt.ylabel("AUC")
+plt.tight_layout()
+plt.legend(loc='best')
+plt.show()
+
+
+def val_curve_params(model, X, y, param_name, param_range, scoring="roc_auc", cv=10):
+
+    train_score, test_score = validation_curve(model, X=X, y=y,
+                                               param_name=param_name,
+                                               param_range=param_range,
+                                               scoring=scoring,
+                                               cv=cv)
+
+    mean_train_score = np.mean(train_score, axis=1)
+    mean_test_score = np.mean(test_score, axis=1)
+
+    plt.plot(param_range, mean_train_score,
+             label="Training Score", color='b')
+
+    plt.plot(param_range, mean_test_score,
+             label="Validation Score", color='g')
+
+    plt.title(f"Validation Curve for {type(model).__name__}") 
+    plt.xlabel(f"Number of {param_name}") 
+    plt.ylabel(f"{scoring}") 
+    plt.tight_layout()
+    plt.legend(loc='best')
+    plt.show(block=True)
+
+val_curve_params(cart_final, X, y, "max_depth", range(1, 11), scoring="f1")
+cart_val_params = [["max_depth", range(1, 11)], ["min_samples_split", range(2, 20)]]
+
+for i in range(len(cart_val_params)):
+    val_curve_params(cart_model, X, y, cart_val_params[i][0], cart_val_params[i][1])
+
+
+# Visualizing the Decision Tree
+def tree_graph(model, col_names, file_name):
+    tree_str = export_graphviz(model, feature_names=col_names, filled=True, out_file=None)
+    graph = pydotplus.graph_from_dot_data(tree_str)
+    graph.write_png(file_name)
+
+tree_graph(model=cart_final, col_names=X.columns, file_name="cart_final.png")
+cart_final.get_params()
+
+# Extracting Decision Rules
+tree_rules = export_text(cart_final, feature_names=list(X.columns))
+print(tree_rules)
+
+
+# Extracting Python Codes of Decision Rules
+print(skompile(cart_final.predict).to('python/code')) 
+print(skompile(cart_final.predict).to('sqlalchemy/sqlite')) 
+print(skompile(cart_final.predict).to('excel')) 
+
+
+#Prediction using Python Codes
+def predict_with_rules(x):
+    return ((((((0 if x[0] <= 7.5 else 1) if x[5] <= 30.949999809265137 else 0 if x[6] <=
+    0.5005000084638596 else 0) if x[5] <= 45.39999961853027 else 1 if x[2] <=
+    99.0 else 0) if x[7] <= 28.5 else (1 if x[5] <= 9.649999618530273 else
+    0) if x[5] <= 26.350000381469727 else (1 if x[1] <= 28.5 else 0) if x[1
+    ] <= 99.5 else 0 if x[6] <= 0.5609999895095825 else 1) if x[1] <= 127.5
+     else (((0 if x[5] <= 28.149999618530273 else 1) if x[4] <= 132.5 else
+    0) if x[1] <= 145.5 else 0 if x[7] <= 25.5 else 1 if x[7] <= 61.0 else
+    0) if x[5] <= 29.949999809265137 else ((1 if x[2] <= 61.0 else 0) if x[
+    7] <= 30.5 else 1 if x[6] <= 0.4294999986886978 else 1) if x[1] <=
+    157.5 else (1 if x[6] <= 0.3004999905824661 else 1) if x[4] <= 629.5 else 0
+    ))
+
+X.columns
+x = [12, 13, 20, 23, 4, 55, 12, 7]
+predict_with_rules(x)
+x = [6, 148, 70, 35, 0, 30, 0.62, 50]
+predict_with_rules(x)
+
+
+#Saving and Loading Model
+joblib.dump(cart_final, "cart_final.pkl")
+cart_model_from_disc = joblib.load("cart_final.pkl")
+x = [12, 13, 20, 23, 4, 55, 12, 7]
+cart_model_from_disc.predict(pd.DataFrame(x).T)
